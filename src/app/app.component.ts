bbfe5b0855af3df383e02019190ec84238bb9cdf
Saran <saranmvdm@gmail.com>
2024-02-13 21:29:22 +0530

    added about founder page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeroSectionComponent } from "./pages/hero-section/hero-section.component";
 import { NavbarComponent } from "./pages/navbar/navbar.component";
+import { AboutFounderComponent } from "./pages/about-founder/about-founder.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent, NavbarComponent],
+  imports: [RouterOutlet, HeroSectionComponent, NavbarComponent,AboutFounderComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
--- src/app/pages/about-founder/about-founder.component.ts
+++ src/app/pages/about-founder/about-founder.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-about-founder',
+  selector: 'about-founder',
   standalone: true,
   imports: [],
   templateUrl: './about-founder.component.html',
