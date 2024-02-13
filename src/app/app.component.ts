c38b927250d9471c0e1e403a31128e92a33a5cf4
Saran <saranmvdm@gmail.com>
2024-02-13 21:51:27 +0530

    added why choose us page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from "@angular/router";
 import { HeroSectionComponent } from "./pages/hero-section/hero-section.component";
 import { NavbarComponent } from "./pages/navbar/navbar.component";
 import { AboutFounderComponent } from "./pages/about-founder/about-founder.component";
+import { WhyChooseUsComponent } from "./pages/why-choose-us/why-choose-us.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent, NavbarComponent,AboutFounderComponent],
+  imports: [
+    RouterOutlet,
+    HeroSectionComponent,
+    NavbarComponent,
+    AboutFounderComponent,
+    WhyChooseUsComponent,
+  ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
--- src/app/pages/why-choose-us/why-choose-us.component.ts
+++ src/app/pages/why-choose-us/why-choose-us.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-why-choose-us',
+  selector: 'why-choose-us',
   standalone: true,
   imports: [],
   templateUrl: './why-choose-us.component.html',
